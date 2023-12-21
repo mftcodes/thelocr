deaae4a2f0b6d7d91b338b6dc93020fa1e41f9b1
JeffCodes <jeffcodes@duck.com>
2023-12-20 20:52:41 -0500

    getting results to front-end
    needs clean up

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,2 +1,19 @@
 const apiUri = "http://127.0.0.1:8080/api/";
+const counties = document.getElementById('county');
 const categories = document.getElementById('category');
+const searchBtn = document.getElementById('searchBtn');
+const resContainer = document.getElementById('res-container');
+
+searchBtn.addEventListener("click", search);
+
+function init() {
+    getCategories();
+    getMICounties();
+}
+
+function search() {
+    let cat = Number(categories.value);
+    let county = counties.value;
+
+    searchByCategory(true, county, "MI", cat);
+}
