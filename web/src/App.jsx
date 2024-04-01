6dc75ba16f858088f3b9404d53b06a081c46bbc7
JeffCodes <jeffcodes@duck.com>
2024-04-01 16:03:21 -0400

    Clean up comments

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -13,9 +13,8 @@ export default function App() {
             <Route index element={<Search />} />
             <Route path="detail" element={<Detail />} />
 
-            {/* Using path="*"" means "match anything", so this route
-                acts like a catch-all for URLs that we don't have explicit
-                routes for. */}
+            {/* path="*"" matches anything, so acts as a 
+                catch for things we haven't setup */}
             <Route path="*" element={<NoMatch />} />
           </Route>
         </Routes>
@@ -27,15 +26,10 @@ export default function App() {
 function Layout() {
   return (
     <div>
-      {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
       <div className="pb-2">
         <Header />
       </div>
-
-      {/* An <Outlet> renders whatever child route is currently active,
-          so you can think about this <Outlet> as a placeholder for
-          the child routes we defined above. */}
+      {/* <Outlet> renders current child route component */}
       <Outlet />
     </div>
   );
